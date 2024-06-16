538e8b7954c329ecf4944c7ee4c736acfa9c563a
Shravankumar1989 <Shravansahan@gmail.com>
2024-06-16 17:02:30 +0800

    first commit

---
+++ app.js
@@ -0,0 +1,22 @@
+// Import the Express library, which is used to create web applications in Node.js
+const express = require('express');
+
+// Create an instance of the Express application
+const app = express();
+
+// Define the port number on which the server will listen for requests
+const port = 9000;
+
+// Set up a route for the root URL ('/'). When this route is accessed, the callback function is executed
+app.get('/', (req, res) => {
+  // Send 'Hello World!' as the response to the client
+  res.send('Hello World!');
+  // Print 'Hello World!' to the console
+  console.log('Hello World!');
+});
+
+// Start the server and listen on the specified port
+app.listen(port, () => {
+  // Print a message to the console indicating that the server is running and listening on the specified port
+  console.log(`Example app listening at http://localhost:${port}`);
+});
